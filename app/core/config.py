66f222506881b3ab4aed06bfae64078021140021
wepal <wepal@mail.ru>
2024-10-31 17:26:19 +0300

    [FIX] Изменен логин и пароль редиса по умолчанию

--- app/core/config.py
+++ app/core/config.py
@@ -6,19 +6,21 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+
     model_config = SettingsConfigDict(
         env_file=['../.env', '.env'],
         env_file_encoding='utf-8',
         extra='ignore',
         case_sensitive=True,
     )
+
     API_V1_STR: str = ''
     # SERVER_NAME: str
     # SERVER_HOST: AnyHttpUrl
     HOST: str = '127.0.0.1'
     PORT: int = 8001
 
-    PROJECT_NAME: str
+    PROJECT_NAME: str = 'Default Project Name'
 
     SESSION_EXPIRE_MINUTES: int = 60 * 24 * 7  # 7 seconds
 
@@ -34,8 +36,8 @@ class Settings(BaseSettings):
 
     REDIS_HOST: str = os.getenv('REDIS_URL', '10.244.15.120')
     REDIS_PORT: int = os.getenv('REDIS_URL', 6380)
-    REDIS_LOGIN: str = os.getenv('REDIS_LOGIN', 'admin')
-    REDIS_PASSWORD: str = os.getenv('REDIS_PASSWORD', 'administrator')
+    REDIS_LOGIN: str = os.getenv('REDIS_LOGIN', 'username')
+    REDIS_PASSWORD: str = os.getenv('REDIS_PASSWORD', 'password')
 
     QUEUE_EXPIRE_SEC: int = 24 * 60 * 60
 
